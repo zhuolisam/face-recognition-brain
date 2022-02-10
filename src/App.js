bce725d03559cacd3bdcb13c2c1be04828924c71
zhuolisam <zhuolisam0627@gmail.com>
2022-02-11 01:01:16 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
 
   const onButtonSubmit = () => {
     setImageUrl(input);
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://rocky-meadow-29133.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -105,7 +105,7 @@ function App() {
       .then(response => response.json())
       .then(response => {
         if (input) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://rocky-meadow-29133.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
